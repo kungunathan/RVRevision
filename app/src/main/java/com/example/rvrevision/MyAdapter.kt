3b0bb6f5997106a3e9391418d1b280b0b3b4887d
kungunathan <kungun78@gmail.com>
2022-10-07 20:50:24 +0300

    FEAT Adapter

--- app/src/main/java/com/example/rvrevision/MyAdapter.kt
+++ app/src/main/java/com/example/rvrevision/MyAdapter.kt
@@ -1,6 +1,52 @@
 package com.example.rvrevision
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
 
-class MyAdapter(private val programmingList : ArrayList<ProgrammingLanguage>) : RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+class MyAdapter(private val programmingList : ArrayList<ProgrammingLanguage>) :
+    RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    private lateinit var mListener: onItemClickListener
+
+    interface onItemClickListener{
+        fun onItemClick(position: Int)
+    }
+    fun setOnItemClickListener(listener: onItemClickListener){
+        mListener = listener
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false)
+        return MyViewHolder(itemView, mListener)
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        val currentItem = programmingList[position]
+        holder.titleImage.setImageResource(currentItem.titleImage)
+        holder.tvHeading.text = currentItem.heading
+    }
+
+    override fun getItemCount(): Int {
+       return programmingList.size
+    }
+
+    class MyViewHolder(itemView: View, listener: onItemClickListener) : RecyclerView.ViewHolder(itemView){
+
+        val titleImage : ShapeableImageView = itemView.findViewById(R.id.title_image)
+        val tvHeading : TextView = itemView.findViewById(R.id.tv_heading)
+
+        init {
+           itemView.setOnClickListener{
+               listener.onItemClick(adapterPosition)
+           }
+        }
+
+    }
 }
